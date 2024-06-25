3336a907574d2e46b32282f14ccc03fcf89ddb6a
Leenard <lolleenard0@gmail.com>
2024-06-25 21:24:15 +0800

    doc: update

--- src/app/(admin)/admin/records/page.tsx
+++ src/app/(admin)/admin/records/page.tsx
@@ -3,7 +3,13 @@ import { FC } from "react";
 interface PageProps {}
 
 const Page: FC<PageProps> = ({}) => {
-  return <div>Page Records</div>;
+  return (
+    <div>
+      <h1 className="text-lg font-semibold text-gray-900 sm:text-xl dark:text-gray-50">
+        Records
+      </h1>
+    </div>
+  );
 };
 
 export default Page;
--- src/app/(admin)/admin/settings/page.tsx
+++ src/app/(admin)/admin/settings/page.tsx
@@ -3,7 +3,13 @@ import { FC } from "react";
 interface PageProps {}
 
 const Page: FC<PageProps> = ({}) => {
-  return <div>Page Settings</div>;
+  return (
+    <div>
+      <h1 className="text-lg font-semibold text-gray-900 sm:text-xl dark:text-gray-50">
+        Settings
+      </h1>
+    </div>
+  );
 };
 
 export default Page;
