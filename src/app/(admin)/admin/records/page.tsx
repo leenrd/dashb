2fdbbfd5581c4768dece2cb3819675229f29b236
Leenard <lolleenard0@gmail.com>
2024-06-26 12:01:20 +0800

    feat: records page<table>

--- src/app/(admin)/admin/records/page.tsx
+++ src/app/(admin)/admin/records/page.tsx
@@ -1,13 +1,32 @@
 import { FC } from "react";
-
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
+import { z } from "zod";
+import { promises as fs } from "fs";
+import path from "path";
+import { taskSchema } from "@/mock-data/records-schema";
 interface PageProps {}
 
-const Page: FC<PageProps> = ({}) => {
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "src/mock-data/records-tasks.json")
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+const Page: FC<PageProps> = async ({}) => {
+  const tasks = await getTasks();
   return (
     <div>
       <h1 className="text-lg font-semibold text-gray-900 sm:text-xl dark:text-gray-50">
         Records
       </h1>
+      <div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
     </div>
   );
 };
